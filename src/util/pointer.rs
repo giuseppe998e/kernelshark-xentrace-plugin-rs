11ff547f05e9f7fae04a67b774f95866f76f2fe2
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-09-30 13:34:58 +0200

    fix: pointer utils

--- src/util/pointer.rs
+++ src/util/pointer.rs
@@ -1,10 +1,7 @@
-use std::os::raw::c_void;
-
-pub fn into_raw_ptr<T>(var: *mut T) -> *mut c_void {
-    let var_box = Box::new(var);
-    Box::into_raw(var_box) as *mut _
+pub(crate) fn from_raw_ptr<'a, T>(ptr: *const T) -> Option<&'a T> {
+    unsafe { ptr.as_ref() }
 }
 
-pub fn from_raw_ptr<'a, T>(ptr: *mut T) -> Option<&'a mut T> {
+pub(crate) fn from_raw_ptr_mut<'a, T>(ptr: *mut T) -> Option<&'a mut T> {
     unsafe { ptr.as_mut() }
 }
