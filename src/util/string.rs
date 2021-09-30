0a9de5479db6ce50e4e5e293847581b5a32e8d2d
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-09-30 20:42:10 +0200

    fix: support both String and &str

--- src/util/string.rs
+++ src/util/string.rs
@@ -9,7 +9,7 @@ pub(crate) fn from_str_ptr<'a>(c_buf: *const c_char) -> Result<&'a str, Utf8Erro
     c_str.to_str()
 }
 
-pub(crate) fn into_str_ptr(string: &str) -> *const c_char {
+pub(crate) fn into_str_ptr<T: Into<Vec<u8>>>(string: T) -> *const c_char {
     let c_string = CString::new(string).unwrap();
     let str_bytes = c_string.into_bytes_with_nul().into_boxed_slice();
     Box::into_raw(str_bytes) as _
