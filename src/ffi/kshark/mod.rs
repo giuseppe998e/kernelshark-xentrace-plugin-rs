7ed943d15f76bedc6faec161f3c5c7c9c8bc4bc6
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-10-02 23:17:47 +0200

    Add some kshark ffi constants

--- src/ffi/kshark/mod.rs
+++ src/ffi/kshark/mod.rs
@@ -2,3 +2,10 @@ pub(crate) mod context;
 pub(crate) mod entry;
 pub(crate) mod interface;
 pub(crate) mod stream;
+
+use libc::{c_int, c_ushort};
+
+// Constants
+pub(crate) const KS_PLUGIN_UNTOUCHED_MASK: c_ushort = 1 << 7;
+
+pub(crate) const KS_EMPTY_BIN: c_int = -1;
