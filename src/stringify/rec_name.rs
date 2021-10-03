0237f184e40da7ab364e648eae9072a444275fff
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-10-03 15:27:41 +0200

    stringify WIP

--- src/stringify/rec_name.rs
+++ src/stringify/rec_name.rs
@@ -1,53 +1,55 @@
 use crate::cbind::xen::*;
+use libc::c_uint;
 use xentrace_parser::Event;
 
-fn get_trc_gen_name_str(event: &Event) -> String {
+fn get_trc_gen_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_sched_name_str(event: &Event) -> String {
+fn get_trc_sched_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_dom0op_name_str(event: &Event) -> String {
+fn get_trc_dom0op_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_hvm_name_str(event: &Event) -> String {
+fn get_trc_hvm_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_mem_name_str(event: &Event) -> String {
+fn get_trc_mem_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_pv_name_str(event: &Event) -> String {
+fn get_trc_pv_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_shadow_name_str(event: &Event) -> String {
+fn get_trc_shadow_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_hw_name_str(event: &Event) -> String {
+fn get_trc_hw_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
-fn get_trc_guest_name_str(event: &Event) -> String {
+fn get_trc_guest_name_str(ecode: c_uint) -> String {
     todo!()
 }
 
 pub(crate) fn get_record_name_str(event: &Event) -> String {
-    match event.get_code() & TRC_CLASS {
-        // v if v & TRC_GEN == v => get_trc_gen_name_str(&event),
-        // v if v & TRC_SCHED == v => get_trc_sched_name_str(&event),
-        // v if v & TRC_DOM0OP == v => get_trc_dom0op_name_str(&event),
-        // v if v & TRC_HVM == v => get_trc_hvm_name_str(&event),
-        // v if v & TRC_MEM == v => get_trc_mem_name_str(&event),
-        // v if v & TRC_PV == v => get_trc_pv_name_str(&event),
-        // v if v & TRC_SHADOW == v => get_trc_shadow_name_str(&event),
-        // v if v & TRC_HW == v => get_trc_hw_name_str(&event),
-        // v if v & TRC_GUEST == v => get_trc_guest_name_str(&event),
+    let ecode: c_uint = event.get_code();
+    match ecode & TRC_CLASS {
+        // v if v & TRC_GEN == v => get_trc_gen_name_str(ecode),
+        // v if v & TRC_SCHED == v => get_trc_sched_name_str(ecode),
+        // v if v & TRC_DOM0OP == v => get_trc_dom0op_name_str(ecode),
+        // v if v & TRC_HVM == v => get_trc_hvm_name_str(ecode),
+        // v if v & TRC_MEM == v => get_trc_mem_name_str(ecode),
+        // v if v & TRC_PV == v => get_trc_pv_name_str(ecode),
+        // v if v & TRC_SHADOW == v => get_trc_shadow_name_str(ecode),
+        // v if v & TRC_HW == v => get_trc_hw_name_str(ecode),
+        // v if v & TRC_GUEST == v => get_trc_guest_name_str(ecode),
         v => format!("unknown ({:#010X})", v),
     }
 }
