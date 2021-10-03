78490e09ae5d806ee923fbab4b8afcdaeae973f2
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-10-03 15:29:30 +0200

    stringify WIP

--- src/stringify/rec_info.rs
+++ src/stringify/rec_info.rs
@@ -1,43 +1,45 @@
 use crate::cbind::xen::*;
+use libc::c_uint;
 use xentrace_parser::Event;
 
-fn get_trc_gen_info_str(event: &Event) -> String {
+fn get_trc_gen_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_sched_info_str(event: &Event) -> String {
+fn get_trc_sched_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_dom0op_info_str(event: &Event) -> String {
+fn get_trc_dom0op_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_hvm_info_str(event: &Event) -> String {
+fn get_trc_hvm_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_mem_info_str(event: &Event) -> String {
+fn get_trc_mem_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_pv_info_str(event: &Event) -> String {
+fn get_trc_pv_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_shadow_info_str(event: &Event) -> String {
+fn get_trc_shadow_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_hw_info_str(event: &Event) -> String {
+fn get_trc_hw_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
-fn get_trc_guest_info_str(event: &Event) -> String {
+fn get_trc_guest_info_str(einfo: &Vec<c_uint>) -> String {
     todo!()
 }
 
 pub(crate) fn get_record_info_str(event: &Event) -> String {
+    let einfo: &Vec<c_uint> = event.get_extra();
     match event.get_code() & TRC_CLASS {
         // v if v & TRC_GEN == v => get_trc_gen_info_str(&event),
         // v if v & TRC_SCHED == v => get_trc_sched_info_str(&event),
@@ -48,8 +50,7 @@ pub(crate) fn get_record_info_str(event: &Event) -> String {
         // v if v & TRC_SHADOW == v => get_trc_shadow_info_str(&event),
         // v if v & TRC_HW == v => get_trc_hw_info_str(&event),
         // v if v & TRC_GUEST == v => get_trc_guest_info_str(&event),
-        _ => event
-            .get_extra()
+        _ => einfo
             .iter()
             .map(|v| format!("{:#010X}", v))
             .collect::<Vec<String>>()
