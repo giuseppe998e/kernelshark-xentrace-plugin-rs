75b18b868aed2835a81005921a52533dbc116a8f
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-10-04 15:51:55 +0200

    stringify WIP

---
+++ src/stringify/rec_name/mod.rs
@@ -0,0 +1,96 @@
+mod hvm;
+mod hw;
+mod sched;
+
+use crate::{
+    cbind::xen::*,
+    stringify::rec_name::{hvm::get_hvm_name_str, hw::get_hw_name_str, sched::get_sched_name_str},
+};
+use xentrace_parser::{Event, EventCode};
+
+fn get_gen_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x001 => Some("lost_records"),
+        0x002 => Some("wrap_buffer"),
+        0x003 => unreachable!(), /* cpu_change */
+        0x004 => Some("trace_irq"),
+        _ => None,
+    }
+}
+
+fn get_dom0op_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x001 => Some("domain_create"),
+        0x002 => Some("domain_destroy"),
+        _ => None,
+    }
+}
+
+fn get_mem_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x001 => Some("page_grant_map"),
+        0x002 => Some("page_grant_unmap"),
+        0x003 => Some("page_grant_transfer"),
+        _ => None,
+    }
+}
+
+fn get_pv_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() & 0x00F {
+        0x001 => Some("hypercall"),
+        0x003 => Some("trap"),
+        0x004 => Some("page_fault"),
+        0x005 => Some("forced_invalid_op"),
+        0x006 => Some("emulate_privop"),
+        0x007 => Some("emulate_4G"),
+        0x008 => Some("math_state_restore"),
+        0x009 => Some("paging_fixup"),
+        0x00A => Some("gdt_ldt_mapping_fault"),
+        0x00B => Some("ptwr_emulation"),
+        0x00C => Some("ptwr_emulation_pae"),
+        0x00D | 0x00E => Some("hypercall"),
+        _ => None,
+    }
+}
+
+fn get_shadow_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() & 0x00F {
+        0x001 => Some("shadow_not_shadow"),
+        0x002 => Some("shadow_fast_propagate"),
+        0x003 => Some("shadow_fast_mmio"),
+        0x004 => Some("shadow_false_fast_path"),
+        0x005 => Some("shadow_mmio"),
+        0x006 => Some("shadow_fixup"),
+        0x007 => Some("shadow_domf_dying"),
+        0x008 => Some("shadow_emulate"),
+        0x009 => Some("shadow_emulate_unshadow_user"),
+        0x00A => Some("shadow_emulate_unshadow_evtinj"),
+        0x00B => Some("shadow_emulate_unshadow_unhandled"),
+        0x00C => Some("shadow_emulate_wrmap_bf"),
+        0x00D => Some("shadow_emulate_prealloc_unpin"),
+        0x00E => Some("shadow_emulate_resync_full"),
+        0x00F => Some("shadow_emulate_resync_only"),
+        _ => None,
+    }
+}
+
+pub(crate) fn get_record_name_str(event: &Event) -> String {
+    let ecode = event.get_code();
+    let result_str = match ecode.get_main() {
+        TRC_GEN => get_gen_name_str(ecode),
+        TRC_SCHED => get_sched_name_str(ecode),
+        TRC_DOM0OP => get_dom0op_name_str(ecode),
+        TRC_HVM => get_hvm_name_str(ecode),
+        TRC_MEM => get_mem_name_str(ecode),
+        TRC_PV => get_pv_name_str(ecode),
+        TRC_SHADOW => get_shadow_name_str(ecode),
+        TRC_HW => get_hw_name_str(ecode),
+        TRC_GUEST => Some("TRC_GUEST"),
+        _ => None,
+    };
+
+    match result_str {
+        Some(v) => v.to_owned(),
+        None => format!("unknown ({:#010X})", ecode.into_u32()),
+    }
+}
---
+++ src/stringify/rec_name/sched.rs
@@ -0,0 +1,106 @@
+use xentrace_parser::EventCode;
+
+fn get_sched_min_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x002 => Some("continue_running"),
+        0x011 => Some("running_to_runnable"),
+        0x021 => Some("running_to_blocked"),
+        0x031 => Some("running_to_offline"),
+        0x101 => Some("runnable_to_running"),
+        0x121 => Some("runnable_to_blocked"),
+        0x131 => Some("runnable_to_offline"),
+        0x201 => Some("blocked_to_running"),
+        0x211 => Some("blocked_to_runnable"),
+        0x231 => Some("blocked_to_offline"),
+        0x301 => Some("offline_to_running"),
+        0x311 => Some("offline_to_runnable"),
+        0x321 => Some("offline_to_blocked"),
+        _ => None,
+    }
+}
+
+fn get_sched_class_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x001 => Some("csched:sched_tasklet"),
+        0x002 => Some("csched:account_start"),
+        0x003 => Some("csched:account_stop"),
+        0x004 => Some("csched:stolen_vcpu"),
+        0x005 => Some("csched:picked_cpu"),
+        0x006 => Some("csched:tickle"),
+        0x007 => Some("csched:boost"),
+        0x008 => Some("csched:unboost"),
+        0x009 => Some("csched:schedule"),
+        0x00A => Some("csched:ratelimit"),
+        0x00B => Some("csched:steal_check"),
+
+        0x201 => Some("csched2:tick"),
+        0x202 => Some("csched2:runq_pos"),
+        0x203 => Some("csched2:credit"),
+        0x204 => Some("csched2:credit_add"),
+        0x205 => Some("csched2:tickle_check"),
+        0x206 => Some("csched2:tickle"),
+        0x207 => Some("csched2:credit_reset"),
+        0x208 => Some("csched2:sched_tasklet"),
+        0x209 => Some("csched2:update_load"),
+        0x20A => Some("csched2:runq_assign"),
+        0x20B => Some("csched2:updt_vcpu_load"),
+        0x20C => Some("csched2:updt_runq_load"),
+        0x20D => Some("csched2:tickle_new"),
+        0x20E => Some("csched2:runq_max_weight"),
+        0x20F => Some("csched2:migrrate"),
+        0x210 => Some("csched2:load_check"),
+        0x211 => Some("csched2:load_balance"),
+        0x212 => Some("csched2:pick_cpu"),
+        0x213 => Some("csched2:runq_candidate"),
+        0x214 => Some("csched2:schedule"),
+        0x215 => Some("csched2:ratelimit"),
+        0x216 => Some("csched2:runq_cand_chk"),
+
+        0x801 => Some("rtds:tickle"),
+        0x802 => Some("rtds:runq_pick"),
+        0x803 => Some("rtds:burn_budget"),
+        0x804 => Some("rtds:repl_budget"),
+        0x805 => Some("rtds:sched_tasklet"),
+        0x806 => Some("rtds:schedule"),
+
+        0xA01 => Some("null:pick_cpu"),
+        0xA02 => Some("null:assign"),
+        0xA03 => Some("null:deassign"),
+        0xA04 => Some("null:migrate"),
+        0xA05 => Some("null:schedule"),
+        0xA06 => Some("null:sched_tasklet"),
+        _ => None,
+    }
+}
+
+fn get_sched_verbose_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_minor() {
+        0x001 => Some("sched_add_domain"),
+        0x002 => Some("sched_rem_domain"),
+        0x003 => Some("domain_sleep"),
+        0x004 => Some("domain_wake"),
+        0x005 => Some("do_yield"),
+        0x006 => Some("do_block"),
+        0x007 => Some("domain_shutdown"),
+        0x008 => Some("sched_ctl"),
+        0x009 => Some("sched_adjdom"),
+        0x00A => Some("__enter_scheduler"),
+        0x00B => Some("s_timer_fn"),
+        0x00C => Some("t_timer_fn"),
+        0x00D => Some("dom_timer_fn"),
+        0x00E => Some("switch_infprev"),
+        0x00F => Some("switch_infnext"),
+        0x010 => Some("domain_shutdown_code"),
+        0x011 => Some("switch_infcont"),
+        _ => None,
+    }
+}
+
+pub(super) fn get_sched_name_str<'a>(ecode: EventCode) -> Option<&'a str> {
+    match ecode.get_sub() {
+        0x1 => get_sched_min_name_str(ecode),
+        0x2 => get_sched_class_name_str(ecode),
+        0x8 => get_sched_verbose_name_str(ecode),
+        _ => None,
+    }
+}
