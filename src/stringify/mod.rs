81c79af9b0327d6f85727877a7d6339a27ec1834
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-10-05 20:14:14 +0200

    update "get_record_task_str" fn

--- src/stringify/mod.rs
+++ src/stringify/mod.rs
@@ -7,14 +7,14 @@ pub(crate) use rec_name::get_record_name_str;
 use xentrace_parser::{Domain, DomainType};
 
 pub(crate) fn get_record_task_str(domain: &Domain) -> String {
-    if domain.get_type() != DomainType::Default {
-        let dom_str: String = match domain.get_type() {
-            DomainType::Idle => "idle".to_owned(),
-            not_idle => format!("d{}", not_idle.into_id()),
-        };
-
-        format!("{}/v{}", dom_str, domain.get_vcpu())
-    } else {
-        "default/v?".to_owned()
+    if domain.get_type() == DomainType::Default {
+        return "default/v?".to_owned();
     }
+
+    let dom_str: String = match domain.get_type() {
+        DomainType::Idle => "idle".to_owned(),
+        not_idle => format!("d{}", not_idle.into_id()),
+    };
+
+    format!("{}/v{}", dom_str, domain.get_vcpu())
 }
