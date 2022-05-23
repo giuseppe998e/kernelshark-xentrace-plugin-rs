94e2e28cb389bb2f970331861edd1f0c761b4414
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-05-23 18:14:04 +0200

    Minor changes

--- src/cbind/mod.rs
+++ src/cbind/mod.rs
@@ -1,2 +1,5 @@
 pub(crate) mod kshark;
 pub(crate) mod xen;
+
+// N.B. Use '(cargo clean) && (rustup run nightly cargo rustc -- -Zprint-type-sizes > sizes.txt)'
+// to print the sizes of the project structures.
