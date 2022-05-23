0aab6e4e279126719348d2ec03125855789ca0f3
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-05-23 22:35:46 +0200

    Minor changes

--- src/stringify/rec_info/mod.rs
+++ src/stringify/rec_info/mod.rs
@@ -40,15 +40,15 @@ fn get_trc_guest_info_str(_event: &Event) -> Option<&str> {
 pub(crate) fn get_record_info_str(event: &Event) -> String {
     let ecode = event.code;
     let result_str = match ecode.main {
-        TRC_GEN => get_trc_gen_info_str(&event),
-        TRC_SCHED => get_trc_sched_info_str(&event),
-        TRC_DOM0OP => get_trc_dom0op_info_str(&event),
-        TRC_HVM => get_trc_hvm_info_str(&event),
-        TRC_MEM => get_trc_mem_info_str(&event),
-        TRC_PV => get_trc_pv_info_str(&event),
-        TRC_SHADOW => get_trc_shadow_info_str(&event),
-        TRC_HW => get_trc_hw_info_str(&event),
-        TRC_GUEST => get_trc_guest_info_str(&event),
+        TRC_GEN => get_trc_gen_info_str(event),
+        TRC_SCHED => get_trc_sched_info_str(event),
+        TRC_DOM0OP => get_trc_dom0op_info_str(event),
+        TRC_HVM => get_trc_hvm_info_str(event),
+        TRC_MEM => get_trc_mem_info_str(event),
+        TRC_PV => get_trc_pv_info_str(event),
+        TRC_SHADOW => get_trc_shadow_info_str(event),
+        TRC_HW => get_trc_hw_info_str(event),
+        TRC_GUEST => get_trc_guest_info_str(event),
         _ => None,
     };
 
